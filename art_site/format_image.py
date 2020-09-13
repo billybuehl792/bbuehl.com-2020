fb228835556b4c22f227249dafe83187db68e371
Billy Buehl <billybuehl792@gmail.com>
2020-09-13 19:03:40 -0400

    initial commit

---
+++ art_site/format_image.py
@@ -0,0 +1,13 @@
+# image_format.py - format image for web view
+
+from PIL import Image
+
+
+class FormatImage():
+
+    def __init__(self, img):
+        self.img = img
+
+    def create_thumbnail(self):
+        pass
+
